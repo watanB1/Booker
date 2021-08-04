1c867cd3adb0b704ffdc343fae9cb8fb012208d6
ou k <watabekoutarou1123@gmail.com>
2021-08-04 15:58:53 +0900

    fix

--- omikuji.js
+++ omikuji.js
@@ -1,27 +1,28 @@
-var username ;
+var username;
 var userresult;
-
-username = prompt("お名前を教えて下さい。");
-document.getElementById("name").interHTML = username;
-
-if(username == "") {
-    username = "名無し"
+username=prompt("お名前を教えてください。");
+if (username==""){
+    username="名無し";
 }
 
-var rand = Math.floor() *5) ;
+document.getElementById("name").innerHTML = username;
+
+
+var rand = Math.floor(Math.random()*5);
 if (rand == 0) {
     userresult = "大吉";
-}
-if (rand == 1) {
+  }
+  if (rand == 1) {
     userresult = "中吉";
-}
-if (rand == 2) {
+  }
+  if (rand == 2) {
     userresult = "小吉";
-}
-if (rand == 3){
+  }
+  if (rand == 3) {
     userresult = "吉";
-}
-if(rand == 4){
-    userresult ="凶"
-}
-document.getElementById("result").innerHTML = userresult;
+  }
+  if (rand == 4) {
+    userresult = "凶";
+  }
+
+  document.getElementById("result").innerHTML=userresult;
